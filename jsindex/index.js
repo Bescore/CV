ab0d20a6ea5206b4cc85a5c19b4890eaff537548
Bescore <jeanmar-97.1@hotmail.fr>
2022-03-25 15:00:49 +0100

    scroll top ajouté !

--- jsindex/index.js
+++ jsindex/index.js
@@ -1,14 +1,23 @@
 const burger = document.getElementById( "burger" );
 const nav = document.querySelector( ".navig" );
+const goUp =document.querySelector(".arrow")
 
-
+// activer le menu smartphone
 function uNmenu () {
     nav.style.transform = "translateX( -200px )";
 }
 function menu () {
     nav.style.transform = "translateX( 0px )";
-   setTimeout(uNmenu,3000)
+   setTimeout(uNmenu,4000)
 }
 
+// remonter en haut de la page
 
+function scroltop () {
+    window.scroll( {
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    } );
+}
     
